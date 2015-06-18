606f3be6d0959add08371d1100b459e97cb2a123
Riemer Grootjans <riemer.grootjans@gmail.com>
2015-06-18 21:17:58 +0200

    Digital bus decoder

---
+++ Default/DecoderBus.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using LabNation.Interfaces;
+
+namespace LabNation.Decoders
+{
+    [Export(typeof(IDecoder))]
+    public class DecoderBus : IDecoder
+    {
+        public DecoderDescription Description
+        {
+            get
+            {
+                return new DecoderDescription()
+                {
+                    Name = "Digital Bus",
+                    ShortName = "BUS",
+                    Author = "LabNation",
+                    VersionMajor = 0,
+                    VersionMinor = 1,
+                    Description = "Bus decoder, converting 8 bits into a readable value",
+                    InputWaveformTypes = new Dictionary<string, Type>() 
+                    {
+                        { "B0", typeof(bool?)},
+                        { "B1", typeof(bool?)},
+                        { "B2", typeof(bool?)},
+                        { "B3", typeof(bool?)},
+                        { "B4", typeof(bool?)},
+                        { "B5", typeof(bool?)},
+                        { "B6", typeof(bool?)},
+                        { "B7", typeof(bool?)}
+                    },
+                    Parameters = null
+                };
+            }
+        }
+
+        public DecoderOutput[] Decode(Dictionary<string, Array> inputWaveforms, Dictionary<string, object> parameters, double samplePeriod)
+        {
+            //name input waveforms for easier usage
+            bool[] b0 = (bool[])inputWaveforms["B0"];
+            bool[] b1 = (bool[])inputWaveforms["B1"];
+            bool[] b2 = (bool[])inputWaveforms["B2"];
+            bool[] b3 = (bool[])inputWaveforms["B3"];
+            bool[] b4 = (bool[])inputWaveforms["B4"];
+            bool[] b5 = (bool[])inputWaveforms["B5"];
+            bool[] b6 = (bool[])inputWaveforms["B6"];
+            bool[] b7 = (bool[])inputWaveforms["B7"];
+
+            //first convert all bool values into ushorts
+            int[] convertedValues = new int[b0.Length];
+            for (int i = 0; i < convertedValues.Length; i++)
+                convertedValues[i] = (b0[i] ? 1 : 0) + (b1[i] ? 2 : 0) + (b2[i] ? 4 : 0) + (b3[i] ? 8 : 0) + (b4[i] ? 16 : 0) + (b5[i] ? 32 : 0) + (b6[i] ? 64 : 0) + (b7[i] ? 128 : 0);
+
+            //initialize output structure
+            List<DecoderOutput> decoderOutputList = new List<DecoderOutput>();
+
+            int startIndex = 0;
+            bool toggle = false;
+            for (int i = 1; i < convertedValues.Length; i++)
+            {
+                if (convertedValues[i] != convertedValues[i - 1])
+                {
+                    DecoderOutputColor color = toggle ? DecoderOutputColor.Green : DecoderOutputColor.Purple;
+                    decoderOutputList.Add(new DecoderOutputValue<byte>(startIndex, i, color, (byte)convertedValues[i-1], ""));
+                    startIndex = i;
+                    toggle = !toggle;
+                }
+            }
+
+            return decoderOutputList.ToArray();
+        }
+    }
+}
